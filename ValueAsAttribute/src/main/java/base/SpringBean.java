f8d6dcdd743e67015e594496c8b04ee66baa6947
SAM <sam062>
2020-02-20 17:32:35 +0530

    Added How to work with value as Attribute.

---
+++ ValueAsAttribute/src/main/java/base/SpringBean.java
@@ -0,0 +1,35 @@
+package base;
+
+public class SpringBean {
+	private int id;
+	private String name;
+	private double fee;
+	public SpringBean() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getFee() {
+		return fee;
+	}
+	public void setFee(double fee) {
+		this.fee = fee;
+	}
+	@Override
+	public String toString() {
+		return "SpringBean [id=" + id + ", name=" + name + ", fee=" + fee + "]";
+	}
+	
+	
+
+}
